055d3af88e8b7dc4fbd9d43c7d13b8151fd13d3a
ayuram <54257595+ayuram@users.noreply.github.com>
2021-01-01 03:36:24 -0800

    backend code adjusted for UI

--- app/src/main/java/com/cuttlefish/teamtrackandroid/endgameScoring.kt
+++ app/src/main/java/com/cuttlefish/teamtrackandroid/endgameScoring.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
@@ -13,17 +13,17 @@ class endgameScoring : Fragment() {
     private var wobblesDrop = 0
     private var wobblesStart = 0
     private var wobbleRings = 0
-    private var powerShotUp: ImageButton? = null
-    private var powerShotDown: ImageButton? = null
+    private var powerShotUp: Button? = null
+    private var powerShotDown: Button? = null
     private var endgamePowerShotScore: TextView? = null
-    private var wobblesDropUp: ImageButton? = null
-    private var wobblesDropDown: ImageButton? = null
+    private var wobblesDropUp: Button? = null
+    private var wobblesDropDown: Button? = null
     private var wobblesDropScore: TextView? = null
-    private var wobblesStartUp: ImageButton? = null
-    private var wobblesStartDown: ImageButton? = null
+    private var wobblesStartUp: Button? = null
+    private var wobblesStartDown: Button? = null
     private var wobblesStartScore: TextView? = null
-    private var wobbleRingsUp: ImageButton? = null
-    private var wobbleRingsDown: ImageButton? = null
+    private var wobbleRingsUp: Button? = null
+    private var wobbleRingsDown: Button? = null
     private var wobbleRingsScore: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,17 +31,17 @@ class endgameScoring : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.endgame_scoring, container, false)
-        powerShotUp = view.findViewById<View>(R.id.endgamePowerShotsUp) as ImageButton
-        powerShotDown = view.findViewById<View>(R.id.endgamePowerShotsDown) as ImageButton
+        powerShotUp = view.findViewById<View>(R.id.endgamePowerShotsUp) as Button
+        powerShotDown = view.findViewById<View>(R.id.endgamePowerShotsDown) as Button
         endgamePowerShotScore = view.findViewById<View>(R.id.endgamePowerShots) as TextView
-        wobblesDropUp = view.findViewById<View>(R.id.wobblesDropUp) as ImageButton
-        wobblesDropDown = view.findViewById<View>(R.id.wobblesDrop) as ImageButton
+        wobblesDropUp = view.findViewById<View>(R.id.wobblesDropUp) as Button
+        wobblesDropDown = view.findViewById<View>(R.id.wobblesDrop) as Button
         wobblesDropScore = view.findViewById<View>(R.id.wobblesDrop) as TextView
-        wobblesStartUp = view.findViewById<View>(R.id.wobblesStartUp) as ImageButton
-        wobblesStartDown = view.findViewById<View>(R.id.wobblesStartDown) as ImageButton
+        wobblesStartUp = view.findViewById<View>(R.id.wobblesStartUp) as Button
+        wobblesStartDown = view.findViewById<View>(R.id.wobblesStartDown) as Button
         wobblesStartScore = view.findViewById<View>(R.id.wobblesStart) as TextView
-        wobbleRingsUp = view.findViewById<View>(R.id.wobbleRingsUp) as ImageButton
-        wobbleRingsDown = view.findViewById<View>(R.id.wobbleRingsDown) as ImageButton
+        wobbleRingsUp = view.findViewById<View>(R.id.wobbleRingsUp) as Button
+        wobbleRingsDown = view.findViewById<View>(R.id.wobbleRingsDown) as Button
         wobbleRingsScore = view.findViewById<View>(R.id.wobbleRings) as TextView
         powerShotUp!!.setOnClickListener {
             endgamePowerShotScore!!.text = Integer.toString(++endgamePowerShots)
--- app/src/main/java/com/cuttlefish/teamtrackandroid/teleScoring.kt
+++ app/src/main/java/com/cuttlefish/teamtrackandroid/teleScoring.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
@@ -12,14 +12,14 @@ class teleScoring : Fragment() {
     private var highGoals = 0
     private var midGoals = 0
     private var lowGoals = 0
-    private var highGoalUp: ImageButton? = null
-    private var highGoalDown: ImageButton? = null
+    private var highGoalUp: Button? = null
+    private var highGoalDown: Button? = null
     private var highGoalScore: TextView? = null
-    private var midGoalUp: ImageButton? = null
-    private var midGoalDown: ImageButton? = null
+    private var midGoalUp: Button? = null
+    private var midGoalDown: Button? = null
     private var midGoalScore: TextView? = null
-    private var lowGoalUp: ImageButton? = null
-    private var lowGoalDown: ImageButton? = null
+    private var lowGoalUp: Button? = null
+    private var lowGoalDown: Button? = null
     private var lowGoalScore: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,14 +27,14 @@ class teleScoring : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.tele_scoring, container, false)
-        highGoalUp = view.findViewById<View>(R.id.teleHighGoalUp) as ImageButton
-        highGoalDown = view.findViewById<View>(R.id.teleHighGoalDown) as ImageButton
+        highGoalUp = view.findViewById<View>(R.id.teleHighGoalUp) as Button
+        highGoalDown = view.findViewById<View>(R.id.teleHighGoalDown) as Button
         highGoalScore = view.findViewById<View>(R.id.teleHighGoals) as TextView
-        midGoalUp = view.findViewById<View>(R.id.teleMiddleGoalUp) as ImageButton
-        midGoalDown = view.findViewById<View>(R.id.teleMiddleGoalDown) as ImageButton
+        midGoalUp = view.findViewById<View>(R.id.teleMiddleGoalUp) as Button
+        midGoalDown = view.findViewById<View>(R.id.teleMiddleGoalDown) as Button
         midGoalScore = view.findViewById<View>(R.id.teleMiddleGoals) as TextView
-        lowGoalUp = view.findViewById<View>(R.id.teleLowGoalUp) as ImageButton
-        lowGoalDown = view.findViewById<View>(R.id.teleLowGoalDown) as ImageButton
+        lowGoalUp = view.findViewById<View>(R.id.teleLowGoalUp) as Button
+        lowGoalDown = view.findViewById<View>(R.id.teleLowGoalDown) as Button
         lowGoalScore = view.findViewById<View>(R.id.teleLowGoals) as TextView
         highGoalUp!!.setOnClickListener { highGoalScore!!.text = Integer.toString(++highGoals) }
         highGoalDown!!.setOnClickListener { highGoalScore!!.text = Integer.toString(--highGoals) }
