e4a0a647528efbd435b7664d83e578aa12d735b4
ayuram <54257595+ayuram@users.noreply.github.com>
2021-01-02 23:27:39 -0800

    Update Match.kt

--- app/src/main/java/com/cuttlefish/teamtrackandroid/Match.kt
+++ app/src/main/java/com/cuttlefish/teamtrackandroid/Match.kt
@@ -3,15 +3,15 @@ package com.cuttlefish.teamtrackandroid
 import android.util.Pair
 import java.util.*
 
-class Match(r0: Team, r1: Team, b0: Team, b1: Team, var type: EventType) {
-    var red: Pair<Team, Team> = Pair<Team, Team>(r0, r1)
-    var blue: Pair<Team, Team> = Pair<Team, Team>(b0, b1)
+class Match(r0: Team?, r1: Team?, b0: Team?, b1: Team?, var type: EventType) {
+    var red: Pair<Team?, Team?> = Pair<Team?, Team?>(r0, r1)
+    var blue: Pair<Team?, Team?> = Pair<Team?, Team?>(b0, b1)
     val id = UUID.randomUUID()
 
     init {
-        r0.scores.add(Score(id))
-        r1.scores.add(Score(id))
-        b0.scores.add(Score(id))
-        b1.scores.add(Score(id))
+        r0?.scores?.add(Score(id))
+        r1?.scores?.add(Score(id))
+        b0?.scores?.add(Score(id))
+        b1?.scores?.add(Score(id))
     }
 }
